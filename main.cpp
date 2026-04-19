4538616f18787c1083d2f2d8f4712e3f4cdc2376
takumi-shibamoto <t.shibamoto@digipen.edu>
2026-04-19 17:06:13 +0800

    first window creation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include "Math/Vector2D.h"
-#include "Core/Types.h"
+
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
-    sakura::Vec2 vec2{};
-    std::cout << vec2.x << "," << vec2.y << std::endl;
+    glfwInit();
+
+    //Set the GLFW version to 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //Create the window.
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Sakura Project", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    //Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD." << std::endl;
+        return -1;
+    }
+
+    //Set the viewport.
+    glViewport(0, 0, 800, 600);
+
+    //Set a callback to update the viewport when the window is resized.
+    glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+    //render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;   
 }
